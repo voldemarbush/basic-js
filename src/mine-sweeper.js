a985338c36b6477900d519ea6867e8330777b199
Vladimir Bushev <voldemarbush@gmail.com>
2023-12-17 20:02:20 +0300

    feat: completed task: mine-sweeper (Sun, Dec 17, 2023 20:02:20)

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const result = [];
+  const checkPos = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+  for (row = 0; row < matrix.length; row++) {
+    result.push([]);
+    for (col = 0; col < matrix[1].length; col++) {
+      if (matrix[row][col]) {
+        result[row][col] = 1;
+      } else {
+        let bombCounter = 0;
+        checkPos.forEach((item) => {
+          const checkRow = row + item[0];
+          const checkCol = col + item[1];
+          if (
+            checkRow < 0 ||
+            checkRow > matrix.length - 1 ||
+            checkCol < 0 ||
+            checkCol > matrix[1].length - 1
+          )
+            return;
+          if (matrix[checkRow][checkCol]) bombCounter += 1;
+        });
+        result[row][col] = bombCounter;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
